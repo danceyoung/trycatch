7cfa8cb58163119af4afbd700a4d0c9b498b5a7b
Young <danceyoung@hotmail.com>
2019-04-15 11:20:00 +0800

    Create TryCatching-Bridging-Header.h

---
+++ trycatch-mobileApp/iOS/trycatching/TryCatching-Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  TryCatching-Bridging-Header.h
+//  trycatching
+//
+//  Created by Young on 2019/3/12.
+//  Copyright © 2019 young. All rights reserved.
+//
+
+#ifndef TryCatching_Bridging_Header_h
+#define TryCatching_Bridging_Header_h
+
+#import "XGPush.h"
+// iOS10注册APNs所需头文件
+#ifdef NSFoundationVersionNumber_iOS_9_x_Max
+#import <UserNotifications/UserNotifications.h>
+#endif
+
+#endif /* TryCatching_Bridging_Header_h */
