f7d70adb3ae5758e60e2ed2ca5407607019f5da3
Young <danceyoung@hotmail.com>
2019-04-15 11:20:34 +0800

    Update AppDelegate.swift

--- trycatch-mobileApp/iOS/trycatching/trycatching/AppDelegate.swift
+++ trycatch-mobileApp/iOS/trycatching/trycatching/AppDelegate.swift
@@ -9,21 +9,29 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate, XGPushDelegate {
 
     var window: UIWindow?
 
-    func setRootViewController()->Void{
-        self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+    func setRootViewController(flag: Int)->Void{
+        if flag == 1 {
+            self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
+        }else {
+            window!.rootViewController = LoginViewController()
+        }
     }
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization                 after application launch.
-        if 1==1 {
-            window!.rootViewController = LoginViewController()
+        if UserDefaults.standard.object(forKey: Constant.TTFUID) == nil {
+            setRootViewController(flag: 0)
         }else{
-            setRootViewController()
+            setRootViewController(flag: 1)
         }
+        XGPush.defaultManager().isEnableDebug = true
+        XGPush.defaultManager().notificationConfigure = XGNotificationConfigure(notificationWithCategories: nil, types: [.alert, .sound, .badge])
+        XGPush.defaultManager().startXG(withAppID: Constant.XGAPPID, appKey: Constant.XGAPPSECRET, delegate: self)
+        
         return true
     }
 
@@ -39,6 +47,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("application will enter foreground.")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -48,7 +57,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
 
+    // MARK: - xgPushDelegate
+    func xgPush(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse?, withCompletionHandler completionHandler: @escaping () -> Void) {
+        let temp = response?.notification.request.content.userInfo
+        
+        print("xgPush did receive notification \(temp!)")
+        
+        XGPush.defaultManager().reportXGNotificationResponse(response)
+        completionHandler()
+    }
+    
+    func xgPush(_ center: UNUserNotificationCenter, willPresent notification: UNNotification?, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("app is in forground")
+        
+        guard let userInfo = notification?.request.content.userInfo else {
+            return
+        }
+        
+        XGPush.defaultManager().reportXGNotificationInfo(userInfo)
+        
+    }
+    
+    func xgPushDidRegisteredDeviceToken(_ deviceToken: String?, error: Error?) {
+        guard deviceToken != nil else {
+            print("xgPushDidRegisteredDeviceToken occur error \(error.debugDescription)")
+            return
+        }
+        print("registered device token is \(deviceToken!)")
+    }
+    
+    func xgPushDidFinishStart(_ isSuccess: Bool, error: Error?) {
+        guard isSuccess == true else {
+            print("\(String(describing: error))")
+            return
+        }
+        
+        print("xgPushDidFinishStart is successfully")
+        
+    }
+    
     // MARK: - Split view
 
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
