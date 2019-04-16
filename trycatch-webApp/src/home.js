509a9006b39fff1a0f425efcbb87d9d9098b32f1
Young <danceyoung@hotmail.com>
2019-04-16 14:27:13 +0800

    Update home.js

--- trycatch-webApp/src/home.js
+++ trycatch-webApp/src/home.js
@@ -4,7 +4,7 @@
  * @flow 
  * @Date: 2018-06-21 17:10:49 
  * @Last Modified by: Young
- * @Last Modified time: 2018-10-09 17:34:59
+ * @Last Modified time: 2019-04-16 11:40:30
  */
 import React from "react";
 import { Link } from "react-router-dom";
@@ -14,6 +14,10 @@ import "./constant";
 import Utils from "./utils/utils";
 import md5 from "md5";
 import Header from "./widget/header";
+import nosdksImg from "./imgs/nosdksbox.png";
+import realtimeImg from "./imgs/realtime.png"
+import pushnotifyImg from "./imgs/pushnotification.png"
+import analyzesImg from "./imgs/analyzes.png"
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -70,18 +74,20 @@ export default class Home extends React.Component {
         <Header settingVisible={"hidden"} />
         <div className="ttHomeContent1">
           <div className="ttHomeContent1-left">
-            <p className="ttHomeContent1-left-p">
-              For developers, by developers
-            </p>
+            <p className="ttHomeContent1-left-p">For coders, by coders</p>
             <p />
-            <p>
+            <p style={{ marginRight: 20 }}>
+              Real-time with no special SDKs monitoring, notifying and
+              aggregating application exceptions for coders.
+            </p>
+            {/* <p>
               Try catching exception or other necessary information inside your
               application,
             </p>
             <p>
               once they are catched, immediately send a notification to your
               smartphone.
-            </p>
+            </p> */}
           </div>
           <div className="ttHomeContent1-right">
             <div
@@ -153,13 +159,67 @@ export default class Home extends React.Component {
         </div>
         <section className="ttContent2">
           <div className="ttContent2-div">
-            <p>Try catching</p>
+            <div className="ttContent2-div-image">
+              <img
+                src={nosdksImg}
+                style={{ width: 100, height: 92 }}
+              />
+            </div>
+            <div className="ttContent2-div-right-div">
+              <div className="ttContent2-div-right-div-title">No SDKs</div>
+              <div className="ttContent2-div-right-div-content">
+                You're still under the current coding model, no need to be
+                embed any SDK, you can collect what to be collected.
+              </div>
+            </div>
           </div>
           <div className="ttContent2-div">
-            <p>Try notifying</p>
+            <div className="ttContent2-div-image">
+              <img
+                src={realtimeImg}
+                style={{ width: 100, height: 99 }}
+              />
+            </div>
+            <div className="ttContent2-div-right-div">
+              <div className="ttContent2-div-right-div-title">
+                Real-time
+              </div>
+              <div className="ttContent2-div-right-div-content">
+                Using Apache Flume Source watches the specified files, and
+                tails them in nearly real-time once detected new lines
+                appended to the each files. Customing Flume's HTTP Sink,
+                send those content what you marked to server.
+              </div>
+            </div>
           </div>
           <div className="ttContent2-div">
-            <p>Finally fixed</p>
+            <div className="ttContent2-div-image">
+            <img src={pushnotifyImg} style={{width: 100, height: 103, opacity: 1}}></img>
+            </div>
+            <div className="ttContent2-div-right-div">
+              <div className="ttContent2-div-right-div-title">
+                Push Notifications
+              </div>
+              <div className="ttContent2-div-right-div-content">
+                Now we have TryCatch APP for iOS, you can download, install
+                and login APP, upon receipt of information related you will
+                be immediately sent to your smart phone.
+              </div>
+            </div>
+          </div>
+          <div className="ttContent2-div">
+            <div className="ttContent2-div-image">
+            <img src={analyzesImg} style={{width: 100, height: 99}}></img>
+            </div>
+            <div className="ttContent2-div-right-div">
+              <div className="ttContent2-div-right-div-title">
+                Analyzes and aggregates
+              </div>
+              <div className="ttContent2-div-right-div-content">
+                App and Web all will show the information summary or detail
+                by drawing time chart and other charts.
+              </div>
+            </div>
           </div>
         </section>
       </div>
