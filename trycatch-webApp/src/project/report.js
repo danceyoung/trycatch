a20a0a3c3435ba06e8f82f29e2b7b6e5b69185e4
Young <danceyoung@hotmail.com>
2019-05-13 11:44:32 +0800

    Create report.js

---
+++ trycatch-webApp/src/project/report.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: Young
+ * DSHARP
+ * @flow 
+ * @Date: 2019-05-13 11:27:07 
+ * @Last Modified by: Young
+ * @Last Modified time: 2019-05-13 11:44:05
+ */
+import React from 'react'
+
+export default class Report extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
