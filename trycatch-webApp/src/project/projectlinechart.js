c7d18c890919e671696927ce9735a28627fb09ae
Young <danceyoung@hotmail.com>
2019-05-15 12:51:12 +0800

    implementing piechart
    implementing piechart and optimize layout

--- trycatch-webApp/src/project/projectlinechart.js
+++ trycatch-webApp/src/project/projectlinechart.js
@@ -4,7 +4,7 @@
  * @flow 
  * @Date: 2019-05-13 11:27:07 
  * @Last Modified by: Young
- * @Last Modified time: 2019-05-14 12:37:57
+ * @Last Modified time: 2019-05-15 12:28:00
  */
 import React from 'react'
 import {
@@ -14,7 +14,10 @@ import {
   XAxis,
   Tooltip,
   CartesianGrid,
-  Legend
+  Legend,
+  PieChart,
+  Pie,
+  Cell
 } from "recharts";
 import NetClient from "../network/netclient";
 
@@ -22,7 +25,8 @@ export default class Report extends React.Component {
   constructor(props) {
     super(props)
     this.state={
-      multipleLineChartData:[]
+      multipleLineChartData:[],
+      pieChartData:[],
     }
   }
 
@@ -37,10 +41,13 @@ export default class Report extends React.Component {
       },
       response=>{
         if (response.msg.code === 0) {
+          var per_count = 0
+          var slicePieData = this.state.pieChartData
           var sliceData = this.state.multipleLineChartData
           if (sliceData.length === 0) {
               sliceData = response.chart.map(
                 (yvalue, idx) => {
+                  per_count = per_count + yvalue 
                   var obj = {}
                   obj[propertyName] = yvalue
                   return obj
@@ -48,12 +55,16 @@ export default class Report extends React.Component {
               );
           }else {
             response.chart.map((yvalue, idx)=>{
+              per_count = per_count + yvalue;
               sliceData[idx][propertyName] = yvalue;
             })
           }
+        
+          slicePieData.push({name: propertyName, value: per_count})
 
           this.setState({
-            multipleLineChartData: sliceData
+            multipleLineChartData: sliceData,
+            pieChartData: slicePieData,
           })
         }
       });
@@ -61,10 +72,20 @@ export default class Report extends React.Component {
   }
 
   render() {
-    // console.log("projectlinechart: "+ JSON.stringify(this.state.multipleLineChartData))
+    console.log(
+      "projectlinechart: " + JSON.stringify(this.state.multipleLineChartData)
+    );
+    console.log(
+      "projectpiechart: " + JSON.stringify(this.state.pieChartData)
+    );
     return (
       <div style={{ display: "flex", flex: 1, flexDirection: "column" }}>
-        <LineChart data={this.props.allData} width={670} height={300}>
+        <LineChart
+          data={this.props.allData}
+          width={670}
+          height={300}
+          margin={{ left: -20, right: 20, top: 20 }}
+        >
           <CartesianGrid strokeDasharray="3 3" />
           <YAxis />
           <XAxis />
@@ -77,6 +98,8 @@ export default class Report extends React.Component {
             }}
           />
           <Legend
+            margin={{ top: 0 }}
+            height={40}
             verticalAlign={"top"}
             payload={[
               {
@@ -89,7 +112,7 @@ export default class Report extends React.Component {
           <Line
             type="monotone"
             dataKey="yvalue"
-            stroke="#3B67BC"
+            stroke="lightgray"
             strokeWidth={2}
             label={{
               dy: -15,
@@ -104,12 +127,26 @@ export default class Report extends React.Component {
             marginTop: 20,
             marginBottom: 20,
             display: "flex",
-            height: 2,
-            backgroundColor: "#3B67BC"
+            height: 20,
+            backgroundColor: "#F5F7F9"
           }}
         />
-        <div style={{ display: "flex", flex: 1 }}>
-          <LineChart data={this.state.multipleLineChartData} width={670} height={300}>
+        <div
+          style={{
+            marginBottom: 50,
+            display: "flex",
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <LineChart
+            data={this.state.multipleLineChartData}
+            width={670}
+            height={300}
+            margin={{ left: -20, right: 10 }}
+          >
             <CartesianGrid strokeDasharray="3 3" />
             <YAxis />
             <XAxis />
@@ -117,32 +154,54 @@ export default class Report extends React.Component {
               labelFormatter={name => {
                 return "the " + (24 - name) + "th hour before";
               }}
-              
             />
-            <Legend
-              verticalAlign={"top"}
-            />
-            {
-              this.props.debugers.map((debuger, idx)=>{
-                return (
-                  <Line
-                    key={debuger.user_id}
-                    type="monotone"
-                    dataKey={debuger.alias}
-                    stroke={global.tt_constant.linechart_colors[idx]}
-                    strokeWidth={2}
-                    label={{
-                      dy: -15,
-                      fill: "gray",
-                      fontSize: 14,
-                      textAnchor: "middle"
-                    }}
-                  />
-                );
-              })
-            }
-            
+            <Legend verticalAlign={"top"} height={40} />
+            {this.props.debugers.map((debuger, idx) => {
+              return (
+                <Line
+                  key={debuger.user_id}
+                  type="monotone"
+                  dataKey={debuger.alias}
+                  stroke={global.tt_constant.linechart_colors[idx]}
+                  strokeWidth={2}
+                  label={{
+                    dy: -15,
+                    fill: "gray",
+                    fontSize: 14,
+                    textAnchor: "middle"
+                  }}
+                />
+              );
+            })}
           </LineChart>
+          <PieChart width={670} height={300}>
+            <Legend verticalAlign={"top"} />
+            <Pie
+              data={this.state.pieChartData}
+              cx={335}
+              cy={150}
+              labelLine={true}
+              label={true}
+              // label={renderCustomizedLabel}
+              nameKey="name"
+              outerRadius={120}
+              fill="#8884d8"
+              dataKey="value"
+            >
+              {this.state.pieChartData.map((entry, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={
+                    global.tt_constant.linechart_colors[
+                      this.props.debugers.findIndex(
+                        ele => ele.alias === entry.name
+                      )
+                    ]
+                  }
+                />
+              ))}
+            </Pie>
+          </PieChart>
         </div>
       </div>
     );
