b13513ea40f86ab3a6a5373066a9a1738b4a5eb9
Young <danceyoung@hotmail.com>
2019-06-03 13:02:57 +0800

    Update home.js
    separte projectlist from home.js

--- trycatch-webApp/src/home.js
+++ trycatch-webApp/src/home.js
@@ -1,10 +1,10 @@
 /*
  * @Author: Young
  * DSHARP
- * @flow 
- * @Date: 2018-06-21 17:10:49 
+ * @flow
+ * @Date: 2018-06-21 17:10:49
  * @Last Modified by: Young
- * @Last Modified time: 2019-05-24 15:33:01
+ * @Last Modified time: 2019-05-31 12:06:36
  */
 import React from "react";
 import { Link } from "react-router-dom";
@@ -15,11 +15,11 @@ import Utils from "./utils/utils";
 import md5 from "md5";
 import Header from "./widget/header";
 import nosdksImg from "./imgs/nosdksbox.png";
-import realtimeImg from "./imgs/realtime.png"
-import pushnotifyImg from "./imgs/pushnotification.png"
-import analyzesImg from "./imgs/analyzes.png"
-import sloveImg from "./imgs/slove.png"
-import iOSQRImg from "./imgs/iOSQR.png"
+import realtimeImg from "./imgs/realtime.png";
+import pushnotifyImg from "./imgs/pushnotification.png";
+import analyzesImg from "./imgs/analyzes.png";
+import sloveImg from "./imgs/slove.png";
+import iOSQRImg from "./imgs/iOSQR.png";
 
 export default class Home extends React.Component {
   constructor(props) {
@@ -33,7 +33,10 @@ export default class Home extends React.Component {
   }
 
   componentDidMount() {
-    console.log("Home component did mount, "+sessionStorage.getItem(global.tt_constant.UID))
+    console.log(
+      "Home component did mount, " +
+        sessionStorage.getItem(global.tt_constant.UID)
+    );
   }
 
   _signin(event) {
@@ -52,21 +55,20 @@ export default class Home extends React.Component {
       });
       return;
     }
-    NetClient.netPost(
-      global.tt_constant.net_url_signin,
-      { account_name: this.state.email, password: md5(this.state.password) },
-      response => {
-        if (response.msg.code !== 0) {
-          this.setState({
-            errorAlert: true,
-            errorAlertInfo: global.tt_constant.home_account_pw_nomatch
-          });
-        } else {
-          sessionStorage.setItem(global.tt_constant.UID, response.uid)
-          this.props.history.push("/dashboard/" + response.uid);          
-        }
+    NetClient.netPost(global.tt_constant.net_url_signin, {
+      account_name: this.state.email,
+      password: md5(this.state.password)
+    }).then(response => {
+      if (response.msg.code !== 0) {
+        this.setState({
+          errorAlert: true,
+          errorAlertInfo: global.tt_constant.home_account_pw_nomatch
+        });
+      } else {
+        sessionStorage.setItem(global.tt_constant.UID, response.uid);
+        this.props.history.push("/dashboard/" + response.uid);
       }
-    );
+    });
   }
 
   _onEnterKeyUp(event) {
@@ -87,12 +89,8 @@ export default class Home extends React.Component {
               Real-time with no special SDKs monitoring, notifying and
               aggregating application exceptions for backend coders.
             </p>
-            <div style={{fontSize: 12,}}>
-              <img
-                alt=""
-                src={iOSQRImg}
-                style={{ width: 60, height: 60 }}
-              />
+            <div style={{ fontSize: 12 }}>
+              <img alt="" src={iOSQRImg} style={{ width: 60, height: 60 }} />
               <div>App Store</div>
             </div>
 
@@ -184,9 +182,7 @@ export default class Home extends React.Component {
                 />
               </div>
               <div className="ttContent2-div-right-div">
-                <div className="ttContent2-div-right-div-title">
-                  No SDKs
-                </div>
+                <div className="ttContent2-div-right-div-title">No SDKs</div>
                 <div className="ttContent2-div-right-div-content">
                   You're still under the current coding model, no need to be
                   embed any SDK, you can collect what to be collected.
@@ -202,14 +198,12 @@ export default class Home extends React.Component {
                 />
               </div>
               <div className="ttContent2-div-right-div">
-                <div className="ttContent2-div-right-div-title">
-                  Real-time
-                </div>
+                <div className="ttContent2-div-right-div-title">Real-time</div>
                 <div className="ttContent2-div-right-div-content">
                   Using Apache Flume Source watches the specified files, and
                   tails them in nearly real-time once detected new lines
-                  appended to the each files. Customing Flume's HTTP Sink,
-                  send those content what you marked to server.
+                  appended to the each files. Customing Flume's HTTP Sink, send
+                  those content what you marked to server.
                 </div>
               </div>
             </div>
@@ -229,9 +223,9 @@ export default class Home extends React.Component {
                   Push Notifications
                 </div>
                 <div className="ttContent2-div-right-div-content">
-                  Now we have TryCatch APP for iOS, you can download,
-                  install and login APP, upon receipt of information related
-                  you will be immediately sent to your smart phone.
+                  Now we have TryCatch APP for iOS, you can download, install
+                  and login APP, upon receipt of information related you will be
+                  immediately sent to your smart phone.
                 </div>
               </div>
             </div>
@@ -256,20 +250,16 @@ export default class Home extends React.Component {
           </div>
           <div className="ttContent2-div" style={{ marginLeft: -20 }}>
             <div className="ttContent2-div-image">
-              <img
-                alt=""
-                src={sloveImg}
-                style={{ width: 100, height: 100 }}
-              />
+              <img alt="" src={sloveImg} style={{ width: 100, height: 100 }} />
             </div>
             <div className="ttContent2-div-right-div">
               <div className="ttContent2-div-right-div-title">
                 Finally solve
               </div>
               <div className="ttContent2-div-right-div-content">
-                Errors are found in your applications, finally you will
-                solve these errors. If you can't solve, you can open them to
-                seek helps, for example opening to StackOverFlow.
+                Errors are found in your applications, finally you will solve
+                these errors. If you can't solve, you can open them to seek
+                helps, for example opening to StackOverFlow.
               </div>
             </div>
           </div>
