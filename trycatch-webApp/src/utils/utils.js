3ef77243ae6a83b2742b1677b4ddd7b6c7440c76
Young <danceyoung@hotmail.com>
2019-06-05 15:49:53 +0800

    add getUID function from sessionStorage

--- trycatch-webApp/src/utils/utils.js
+++ trycatch-webApp/src/utils/utils.js
@@ -4,11 +4,15 @@
  * @flow 
  * @Date: 2018-06-22 16:25:19 
  * @Last Modified by: Young
- * @Last Modified time: 2018-06-22 16:25:39
+ * @Last Modified time: 2019-06-05 13:02:52
  */
 export default class Utils {
   static checkEmailValid(emailAddress) {
     var regex = /^(\w-*\.*)+@(\w-?)+(\.\w{2,})+$/;
     return regex.test(emailAddress)
+  }
+
+  static getUID() {
+    return sessionStorage.getItem(global.tt_constant.UID);
   }
 }
